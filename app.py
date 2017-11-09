8ecc3c61905de6f726c1e530c463289b4fd224bf
Tia King <tiajaneeking@gmail.com>
2017-11-09 14:32:45 -0800

    stuff

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask
-import histogram 
+#import histogram
 
 app = Flask(__name__)
 
 @app.route('/')
 def lol():
-	text_file = open('practice_text.txt')
-	text_word_list = [word.lower() for word in text_file.read().rsplit()]
-	text_histogram = histogram.histogram(text_word_list)
-	return histogram.random_word(text_histogram)
+#    text_file = open('practice_text.txt')
+#    text_word_list = [word.lower() for word in text_file.read().rsplit()]
+#    text_histogram = histogram.histogram(text_word_list)
+#    return histogram.random_word(text_histogram)
+    return "hi"
 
 if __name__ == '__main__':
     app.run()
