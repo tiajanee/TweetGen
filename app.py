c9ea992d4ce91e87279a7a133dc5ba09004387a6
Tia King <tiajaneeking@gmail.com>
2017-11-09 14:36:49 -0800

    FORGOT  THE G

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from flask import Flask
-#import histogram
+import histogram
 
 app = Flask(__name__)
 
 @app.route('/')
 def lol():
-#    text_file = open('practice_text.txt')
-#    text_word_list = [word.lower() for word in text_file.read().rsplit()]
-#    text_histogram = histogram.histogram(text_word_list)
-#    return histogram.random_word(text_histogram)
-    return "hi"
+   text_file = open('practice_text.txt')
+   text_word_list = [word.lower() for word in text_file.read().rsplit()]
+   text_histogram = histogram.histogram(text_word_list)
+   return histogram.random_word(text_histogram)
 
 if __name__ == '__main__':
     app.run()
