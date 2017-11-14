f6c17d7c16905b09ca02d386fb39f9c0d7828889
Tia King <tiajaneeking@gmail.com>
2017-11-13 21:39:02 -0800

    Pushed to Heroku

--- app.py
+++ app.py
@@ -8,17 +8,18 @@ def lol():
    text_file = open('practice_text.txt')
    text_word_list = [word.lower() for word in text_file.read().rsplit()]
    text_histogram = histogram.histogram(text_word_list)
+   rand_number = random.randint(0, 15)
+       rand_sentence = " "
+           
+           for i in range(rand_number):
+               if i == rand_number - 1:
+                   rand_sentence += (random_word(text_histogram) + ".")
+                       else:
+                           rand_sentence += (random_word(text_histogram) + " ")
+                               print (rand_sentence)
+                           
    return histogram.random_word(text_histogram)
 
 if __name__ == '__main__':
-    rand_number = random.randint(0, 15)
-    rand_sentence = " "
-    
-    for i in range(rand_number):
-        if i == rand_number - 1:
-            rand_sentence += (random_word(text_histogram) + ".")
-        else:
-            rand_sentence += (random_word(text_histogram) + " ")
-    print (rand_sentence)
     
     app.run()
